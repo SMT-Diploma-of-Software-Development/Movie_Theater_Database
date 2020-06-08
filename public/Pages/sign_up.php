7cff4f78f76f98b92da74588468c07dd17b11685
remiriya <39472998+remiriya@users.noreply.github.com>
2020-06-08 13:23:18 +0800

    Add files via upload

--- public/Pages/sign_up.php
+++ public/Pages/sign_up.php
@@ -32,69 +32,6 @@ if (is_post_request()) {
 
 require SHARED_PATH . '/page_header.php';
 ?>
-<style>
-    body {font-family: Arial, Helvetica, sans-serif;}
-    form {border: 3px solid #f1f1f1;}
-
-    input[type=text], input[type=password] {
-        width: 100%;
-        padding: 12px 20px;
-        margin: 8px 0;
-        display: inline-block;
-        border: 1px solid #ccc;
-        box-sizing: border-box;
-    }
-
-    button {
-        background-color: #4CAF50;
-        color: white;
-        padding: 14px 20px;
-        margin: 8px 0;
-        border: none;
-        cursor: pointer;
-        width: 100%;
-    }
-
-    button:hover {
-        opacity: 0.8;
-    }
-
-    .cancelbtn {
-        width: auto;
-        padding: 10px 18px;
-        background-color: #f44336;
-    }
-
-    .imgcontainer {
-        text-align: center;
-        margin: 24px 0 12px 0;
-    }
-
-    img.avatar {
-        width: 40%;
-        border-radius: 50%;
-    }
-
-    .container {
-        padding: 16px;
-    }
-
-    span.psw {
-        float: right;
-        padding-top: 16px;
-    }
-
-    /* Change styles for span and cancel button on extra small screens */
-    @media screen and (max-width: 300px) {
-        span.psw {
-            display: block;
-            float: none;
-        }
-        .cancelbtn {
-            width: 100%;
-        }
-    }
-</style>
 
 <h2>Subscribe Form</h2>
 
@@ -102,9 +39,9 @@ require SHARED_PATH . '/page_header.php';
     <div class="imgcontainer">
         <img src="../img/img_avatar2.png" alt="Avatar" class="avatar">
     </div>
-<?php echo display_errors($errors); ?>
+    <?php echo display_errors($errors); ?>
     <div class="container">
-        <label for="username"><b>Name</b></label>
+        <label for="username"><b>User Name</b></label>
         <input type="text" placeholder="Enter Username" name="username" required>
 
         <label for="email"><b>Email</b></label>
@@ -119,9 +56,8 @@ require SHARED_PATH . '/page_header.php';
 
 
     <div class="container" style="background-color:#f1f1f1">
-        <a href="<?php echo url_for_public('/Pages/unsubscribe.php'); ?>" class="cancelbtn">Unsubscribe</a>
+        <a href="<?php echo url_for_public('/Pages/unsubscribe.php'); ?>" type="button" class="cancelbtn">Unsubscribe</a>
     </div>
-
 </form>
 
 
