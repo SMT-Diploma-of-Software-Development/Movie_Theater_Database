ac0cb1683cc979ff9fe56c0f8152047067ea166a
g-johnrenald <60998089+g-johnrenald@users.noreply.github.com>
2020-06-15 19:27:00 +0800

    Add files via upload

--- private/staff_portal_auth.php
+++ private/staff_portal_auth.php
@@ -3,7 +3,4 @@ session_start();
 if (!isset($_SESSION['account'])) {
     header("Location: staff_portal_login.php");
     exit();
-} elseif (!$_SESSION['account']['username'] == "admin") {
-    header("Location: staff_portal_login.php");
-    exit();
 }
